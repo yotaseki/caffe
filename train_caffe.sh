be510be3db91a8ef7d42905229ba8532e7c904ff
yotaseki <uyu9480s@outlook.jp>
2017-10-11 05:18:26 +0900

    modify sh

--- train_caffe.sh
+++ train_caffe.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-DATE = `date +"%Y%m%d%k%M%S" |sed "s/ /0/"`
-echo "START" > Logs/$DATE.log
-date >> Logs/$DATE.log
+DATE=`date +"%Y%m%d%k%M%S" |sed "s/ /0/"`
+DATE="caffe.$DATE"
+S1=`date`
 
 ./tools/caffe train -gpu 0 -solver models/whiteline10-fcn8s/solver.prototxt
 
-echo "END" >> Logs/$DATE.log
-date >> Logs/$DATE.log
+echo "START" > Logs/$DATE.log
+echo $S1	>> Logs/$DATE.log
+echo "END"	>> Logs/$DATE.log
+date		>> Logs/$DATE.log
 cat /tmp/caffe.INFO >> Logs/$DATE.log
 
